5041304a4f919685647ba7ec4f047ece39f58750
micoshortpoint <mico@shortpoint.com>
2023-10-27 17:08:59 +0800

    netlify added

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,15 @@ import { defineConfig } from 'astro/config';
 import alpinejs from "@astrojs/alpinejs";
 import tailwind from "@astrojs/tailwind";
 
+import netlify from '@astrojs/netlify/functions';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [alpinejs(), tailwind()],
   site: 'https://micoshortpoint.github.io',
   base: '/new-website-poc-frontend',
+  output: 'server',
+  adapter: netlify({
+    edgeMiddleware: true
+  }), 
 });
