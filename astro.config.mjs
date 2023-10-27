3593f6b9b5368e6c01af17383784d1e6c84b9e03
micoshortpoint <mico@shortpoint.com>
2023-10-27 17:39:53 +0800

    removed astro configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,7 @@ import { defineConfig } from 'astro/config';
 import alpinejs from "@astrojs/alpinejs";
 import tailwind from "@astrojs/tailwind";
 
-import netlify from '@astrojs/netlify/functions';
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [alpinejs(), tailwind()],
-  output: 'server',
-  adapter: netlify({
-    edgeMiddleware: true
-  }), 
 });
